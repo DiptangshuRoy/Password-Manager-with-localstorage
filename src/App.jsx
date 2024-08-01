28f605efb4828726f9691af1683533f960730f0e
DiptangshuRoy <diptangshuroy1234@gmail.com>
2024-08-01 18:05:10 +0530

    solved the Toast nottification stucked problem
    Comment outed the 2nd Toast nottification pop-up component/element from inside the return function in Manager.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
        <Manager/> 
         </div>
-       <Footer/>
+       <Footer />
     </>
   )
 }
--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -92,7 +92,7 @@ const Manager = () => {
             
     }
     const editPassword = (id) => {
-         
+
         console.log("Editing password with id ", id)
         setform(passwordArray.filter(i=>i.id===id)[0]) 
         setPasswordArray(passwordArray.filter(item=>item.id!==id)) 
@@ -109,7 +109,7 @@ const Manager = () => {
 
     return (
         <>
-            <ToastContainer
+            {/* <ToastContainer
                 position="top-right"
                 autoClose={5000}
                 hideProgressBar={false}
@@ -121,7 +121,8 @@ const Manager = () => {
                 pauseOnHover
                 theme="light"
                 transition="Bounce"
-            />
+            /> */}
+
             {/* Same as */}
             <ToastContainer />
             <div className="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-green-400 opacity-20 blur-[100px]"></div></div>
